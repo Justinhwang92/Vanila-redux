9d3deb526d4a757047af5d3e05a306467e53fa76
Justin <justinhwang92@gmail.com>
2021-07-12 21:53:28 -0400

    Add Redux and switch the project to Redux

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
 
-const updateText = () => {
-  number.innerText = count;
+// actions
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+// reducer is a function that modifies your data
+// whatever reducer returns, it will be the state of the application
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
+// store is where you save the data
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+// if you want listen for changes in your store
+// you can subscribe that changes
+countStore.subscribe(onChange);
+
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  // dispatch sends the message to modifier
+  // action must be an object (mush have type)
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
