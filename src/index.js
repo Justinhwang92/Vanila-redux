30f1ecdfe43408334b4020ab3edefd9575e6d7ee
Justin <justinhwang92@gmail.com>
2021-07-14 21:20:37 -0400

    Add Vanilla Todo

--- src/index.js
+++ src/index.js
@@ -1,48 +1,18 @@
-import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-// actions
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-// reducer is a function that modifies your data
-// whatever reducer returns, it will be the state of the application
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
+const createToDo = (toDo) => {
+  const li = document.querySelector("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
 
-// store is where you save the data
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
 };
 
-// if you want listen for changes in your store
-// you can subscribe that changes
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  // dispatch sends the message to modifier
-  // action must be an object (mush have type)
-  countStore.dispatch({ type: ADD });
-};
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
