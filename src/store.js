cdf1424fd0139242c09208dc65b5e0dfe55322ec
Justin <justinhwang92@gmail.com>
2021-07-27 10:16:49 -0400

    Use configureStore

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
@@ -26,7 +26,7 @@ const reducer = createReducer([], {
     state.filter((toDo) => toDo.id !== action.payload), // returning the new state
 });
 
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
